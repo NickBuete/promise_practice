5ad3db03b275becba5f5a21d1e4774b605a11693
Nicholas Buete <nbuete01@gmail.com>
2025-03-22 19:19:29 +1100

    added functionality to app.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Promise Practice
-This is promise practice for simulating 
+This is promise practice for simulating log in for a server.  It should return information about the user, or error if the user is uncredentialled
+ 
--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 function simulateLogin(username, password) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (username === "user" && password ==== "password") {
+            if (username === "user" && password === "password") {
                 resolve('valid-token')
             }
             else {
@@ -19,7 +19,7 @@ function simulateLogin(username, password) {
 //the promise should resolve with the data object (username: , role: )
 //the promise should reject with an error object (error: "invalid token" )
 function simulateFetchData(token) {
-    retturn new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (token === 'valid-token') {
                 resolve({username: 'Admin user', role: 'Administrator'})
@@ -32,5 +32,18 @@ function simulateFetchData(token) {
 }
 
 
-//TODO chain the 
+//TODO chain the login and data fetch
+
+async function loginAndFetchData(username, password) {
+    try {
+        let token = await simulateLogin(username, password)
+        console.log(`login successful with token: ${token}`)
+        let data = await simulateFetchData(token)
+        console.log("data fetched:", data)
+    } catch (error) {
+        console.log('Error ' + error)
+    }
+} 
+
+loginAndFetchData('user', 'password')
 
